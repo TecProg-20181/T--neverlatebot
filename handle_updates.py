0a474fe97af94763a7ef3446998bbe9287901b95
Lucas Kishima <lucaskishima2@gmail.com>
2018-05-02 03:25:51 -0300

    Extracting duplicate_task from handle_updates.

--- handle_updates.py
+++ handle_updates.py
@@ -1,5 +1,5 @@
 import db
-from db import Task
+from db import Task , Association
 
 # TODO(Lucas) Retirar o import abaixo quando a send_message estiver neste arquivo.
 from taskbot import send_message
@@ -15,3 +15,37 @@ def new_task(chat, msg):
     db.session.commit()
 
     send_message("New task *TODO* [[{}]] {}".format(task.id, task.name), chat)
+
+def duplicate_task(chat, msg):
+
+    if not msg.isdigit():
+        send_message("You must inform the task id", chat)
+
+    else:
+        task_id = int(msg)
+        query = db.session.query(Task).filter_by(id=task_id, chat=chat)
+
+        try:
+            task = query.one()
+
+        except sqlalchemy.orm.exc.NoResultFound:
+            send_message("_404_ Task {} not found x.x".format(task_id), chat)
+            return
+
+        duplicated_task = Task(chat=task.chat, name=task.name, status=task.status,
+                     priority=task.priority, duedate=task.duedate,
+                     description=task.description)
+
+        db.session.add(duplicated_task)
+        db.session.commit()
+
+        query = db.session.query(Association).filter_by(parents_id=task_id)
+        query_row = query.one()
+        for query_row in query.all():
+            duplicated_association = Association(id=query_row.id , parents_id=duplicated_task.id)
+            print(type(duplicated_association))
+            print(duplicated_association.parents_id)
+            db.session.add(duplicated_association)
+            db.session.commit()
+
+        send_message("New task *TODO* [[{}]] {}".format(duplicated_task.id, duplicated_task.name), chat)
--- taskbot.py
+++ taskbot.py
@@ -163,37 +163,7 @@ def handle_updates(updates):
                 send_message("Task {} redefined from {} to {}".format(task_id, old_text, text), chat)
 
         elif command == '/duplicate':
-            if not msg.isdigit():
-                send_message("You must inform the task id", chat)
-
-            else:
-                task_id = int(msg)
-                query = db.session.query(Task).filter_by(id=task_id, chat=chat)
-
-                try:
-                    task = query.one()
-
-                except sqlalchemy.orm.exc.NoResultFound:
-                    send_message("_404_ Task {} not found x.x".format(task_id), chat)
-                    return
-
-                duplicated_task = Task(chat=task.chat, name=task.name, status=task.status,
-                             priority=task.priority, duedate=task.duedate,
-                             description=task.description)
-
-                db.session.add(duplicated_task)
-                db.session.commit()
-
-                query = db.session.query(Association).filter_by(parents_id=task_id)
-                query_row = query.one()
-                for query_row in query.all():
-                    duplicated_association = Association(id=query_row.id , parents_id=duplicated_task.id)
-                    print(type(duplicated_association))
-                    print(duplicated_association.parents_id)
-                    db.session.add(duplicated_association)
-                    db.session.commit()
-
-                send_message("New task *TODO* [[{}]] {}".format(duplicated_task.id, duplicated_task.name), chat)
+            duplicate_task(chat, msg)
 
         elif command == '/delete':
             if not msg.isdigit():
