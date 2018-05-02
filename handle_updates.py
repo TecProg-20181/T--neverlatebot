8d871845c02742dfe755de54bbed7d8ab3812be0
Pedro Daniel <pedrodaniel.unb@gmail.com>
2018-05-02 03:15:50 -0300

    #15 - Extracting /rename from handle_updates( )

--- handle_updates.py
+++ handle_updates.py
@@ -1,8 +1,11 @@
+import sqlalchemy
+
 import db
 from db import Task
 
 # TODO(Lucas) Retirar o import abaixo quando a send_message estiver neste arquivo.
 from taskbot import send_message
+
 
 def new_task(chat, msg):
 
@@ -15,3 +18,37 @@ def new_task(chat, msg):
     db.session.commit()
 
     send_message("New task *TODO* [[{}]] {}".format(task.id, task.name), chat)
+
+
+def rename_task(chat, msg):
+    text = ''
+    if msg != '':
+        if len(msg.split(' ', 1)) > 1:
+            text = msg.split(' ', 1)[1]
+        msg = msg.split(' ', 1)[0]
+
+    if not msg.isdigit():
+        send_message("You must inform the task id", chat)
+
+    else:
+        task_id = int(msg)
+        query = db.session.query(Task).filter_by(id=task_id, chat=chat)
+
+        try:
+            task = query.one()
+
+        except sqlalchemy.orm.exc.NoResultFound:
+            send_message(
+                "_404_ Task {} not found x.x".format(task_id), chat)
+            return
+
+        if text == '':
+            send_message(
+                "You want to modify task {}, but you didn't provide any new text".format(task_id), chat)
+            return
+
+        old_text = task.name
+        task.name = text
+        db.session.commit()
+        send_message("Task {} redefined from {} to {}".format(
+            task_id, old_text, text), chat)
